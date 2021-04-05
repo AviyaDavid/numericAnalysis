822dfa63708a333a91e0d1d9db007ed482392aef
AviyaDavid <73593533+AviyaDavid@users.noreply.github.com>
2021-04-05 18:16:59 +0300

    main,det,inverse functions

--- task2.py
+++ task2.py
@@ -1,27 +1,25 @@
 def main():
-    #recieve matrix
+    # recieve matrix
     print('Please enter size for  matrix A :')
     sizeA = input()
-    A= createImat(sizeA)
+    A = createImat(sizeA)
     for i in range(sizeA):
         for j in range(sizeA):
-            print('Please enter value for A['+i+']['+j+']:')
-            A[i][j]= input()
-    B=[]
+            print('Please enter value for A[' + i + '][' + j + ']:')
+            A[i][j] = input()
+    B = []
     for i in range(sizeA):
         print('Please enter value for B[' + i + ']:')
         B.append(input())
 
     # check if inverse= find inverse
-    detA=calcDet(A)
+    detA = calcDet(A)
     if detA is not 0:
-        findInverse(A,detA)
-
-
+        findInverse(A, detA)
 
 
 def createImat(size):
-    newMat=[]
+    newMat = []
     for i in range(size):
         newMat.append([])
         for j in range(size):
@@ -32,81 +30,94 @@ def createImat(size):
     return newMat
 
 
-def MultiplyMatrix(a,b):
-    size= len(a)
-    result=createImat(size) #create unit mat
-    for i in range(size): #reset zeros in result mat
-        result[i][i]=0
-    for i in range(size): #iter on rows
-        for j in range(size): #iter on columns
-            for k in range(size):#iter on columns
-                result[i][j]+=a[i][k]*b[k][i]
-    return result
-
+def MultiplyMatrix(a, b):
+    size = len(a)
+    sum = 0
+    new_mat = []
+    for r in range(size):
+        new_mat.append([])
+        for c in range(size):
+            for k in range(size):
+                sum += (a[r][k] * b[k][c])
+            new_mat[r].append(sum)
+            sum = 0
+    return new_mat
 
 
 def twoontwoDet(mat):
     return (mat[1][1] * mat[2][2]) - (mat[1][2] * mat[2][1])
 
+
 def calcDet(mat):
-    size=len(mat)
+    size = len(mat)
     if size is 2:
         return twoontwoDet(mat)
     if size is 1:
         return mat[1][1]
     else:
-        det=1
-        upperMat=upper(mat)
+        det = 1
+        upperMat = upper(mat)
         for i in range(size):
-            det*=upperMat[i][i]
+            det *= upperMat[i][i]
         return det
 
 
 def upper(mat):
     size = len(mat)
-    elem=createImat(size)
+    elem = createImat(size)
     for i in range(size):
-        pivot=mat[i][i]
+        pivot = mat[i][i]
         for j in range(i, size):
-            elem[j][i]=-(mat[j][i]/pivot)
-        mat= MultiplyMatrix(elem,mat)
+            elem[j][i] = -(mat[j][i] / pivot)
+        mat = MultiplyMatrix(elem, mat)
     return mat
 
-def findElem(mat):
+
+def findInverse(mat, det):
     size = len(mat)
-    elem = createImat(size)
-
-
-def findInverse(mat,det):
-    size=len(mat)
-    #special case for 2x2 matrix
+    # special case for 2x2 matrix
     if size is 2:
-        temp=mat[0][0]
-        mat[0][0]=mat[1][1]
-        mat[1][1]=temp
-        mat[0][1]*=(-1)
+        temp = mat[0][0]
+        mat[0][0] = mat[1][1]
+        mat[1][1] = temp
+        mat[0][1] *= (-1)
         mat[1][0] *= (-1)
-        det=1.0/det
+        det = 1.0 / det
         for i in range(size):
             for j in range(size):
-                mat[i][j]*=det
+                mat[i][j] *= det
         return mat
-
     else:
-
-
-def minorMatrix(mat):
-    size= len(mat)
-    minors=createImat(size)
-    tempMat=createImat(size-1)
-    for row in range(size):
+        inverMat = createImat(size)
         for col in range(size):
-            i=0
-            j=0
-            while i<size:
-                while j<size:
-                    if i != row:
-                        if j != col:
-                            tempMat[i][j]
-                            j+=1
-                        if j==
+            pivot = mat[col][col]
+            tempElem = createImat(size)
+            for row in range(size):
+                if row != col:
+                    tempElem[row][col] = (-1) * (mat[row][col] / pivot)
+            mat = MultiplyMatrix(tempElem, mat)
+            inverMat = MultiplyMatrix(tempElem, inverMat)
+
+        # check diagonal numbers
+        for i in range(size):
+            pivot = mat[i][i]
+            if pivot != 1:
+                for col in range(size):
+                    inverMat[i][col] /= float(pivot)
+                mat[i][i] = 1
+        return inverMat
+
+
+def listElementryMarix(mat):
+    size = len(mat)
+    elemList = []
+    for col in range(size):
+        elem = createImat(size)
+        pivot = mat[col][col]
+        row = 0
+        while row != size:
+            if row != col:
+                elem[row][col] = -(mat[row][col] / pivot)
+            row += 1
+        elemList.append(elem)
+    return elemList
